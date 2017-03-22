61d5630c80f9a3500ec676b7368ef4ae90f7e1b6
Giancarlo Franchini <gfranchini88@gmail.com>
2017-03-22 16:35:08 -0400

    added method to direct user to page based on get request

--- wiki.rb
+++ wiki.rb
@@ -2,6 +2,16 @@ require "sinatra"
 
 set(:bind, '0.0.0.0')
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
 get("/") do
   erb(:welcome) #pulls the welcome.erb file from the views directory
+end
+
+get("/:title") do
+  page_content(params[:title])
 end
